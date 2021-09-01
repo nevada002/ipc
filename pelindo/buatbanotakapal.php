38132b100700bce59a73d57dd1e05d335fa3f150
Juliant Daniel Latuary <daniel.latuary@gmail.com>
2021-09-01 11:23:21 +0700

    .

--- pelindo/buatbanotakapal.php
+++ pelindo/buatbanotakapal.php
@@ -191,12 +191,12 @@
                             </td>
                             <td>
                                 <div class="btn-group open">
-                                    <button type="button" class="btn btn-primary mx-2" >
+                                <button type="button" class="btn btn-primary mx-2" data-toggle="modal" data-target="#viewModal">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
                                         </svg>
-                                    </button>                         
+                                    </button>                          
                                     <button type="button" class="btn btn-success mx-2" data-toggle="modal" data-target="#editModal">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -217,6 +217,16 @@
                                 </div>
                             </td>                
                         </tr>
+                        <!-- Modal -->
+                        <div id="viewModal" class="modal fade" role="dialog">
+                            <div class="modal-dialog modal-dialog-scrollable">
+                                <!-- Modal content-->
+                                <div class="modal-content">                                
+                                <div class="modal-body">                                    
+                                    <img class="w-100 h-100" src="file/Koreksi Nota Kapal.jpeg" alt="" >
+                                </div>
+                            </div>
+                        </div>
                         <!-- Modal -->
                         <div id="editModal" class="modal fade" role="dialog">
                             <div class="modal-dialog modal-dialog-scrollable">
